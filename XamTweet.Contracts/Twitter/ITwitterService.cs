57e860fa91a16b64ee25b5016c3a68ee82284395
Dario Alonso Cerezo <dariovillar@hotmail.com>
2018-07-23 20:43:10 +0200

    Creando servicio para conexión y publicación en Twitter

---
+++ XamTweet.Contracts/Twitter/ITwitterService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reactive;
+using Tweetinvi.Models;
+
+namespace XamTweet.Contracts
+{
+    public interface ITwitterService
+    {
+        IObservable<Unit> Login();
+        IObservable<ITweet> PublishTweet(string tweet);
+    }
+}
---
+++ XamTweet.Services/Twitter/TwitterService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using Tweetinvi;
+using Tweetinvi.Models;
+using XamTweet.Contracts;
+
+namespace XamTweet.Services
+{
+    public class TwitterService : ITwitterService
+    {
+        public IObservable<Unit> Login()
+        {
+            Auth.SetUserCredentials("UVht5bIEPfPy92X4NhSPyoUUh", "cWsJJ3jdXOJJfiCW7JnqnSvxcC00o5DmwoV1szKn8jMoFWAEI0",
+                "184009490-e7veAUSvHbRIQ4xo52TGSBMRabSi1iq5ziiue4ZE", "x1YKaW6BLWhGQdwONee8HihbHaPgIJ4q1aRjEHj454At7");
+            return Observable.Return(Unit.Default);
+        }
+
+        public IObservable<ITweet> PublishTweet(string tweet)
+        {
+            return Observable.Return(Tweet.PublishTweet(tweet));
+        }
+    }
+}
