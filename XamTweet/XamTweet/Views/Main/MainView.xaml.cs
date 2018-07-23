64767f74776c3cd4e431ac10098bcb46b52c6a4c
Dario Alonso Cerezo <dariovillar@hotmail.com>
2018-07-23 20:44:49 +0200

    Cambiando vista inicial y haciendo bindings con el ViewModel

--- XamTweet/XamTweet/Views/Main/MainView.xaml.cs
+++ XamTweet/XamTweet/Views/Main/MainView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using System.Reactive;
+using ReactiveUI;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Xamarin.Forms.Xaml;
 
 namespace XamTweet.Forms.Views
 {
@@ -8,6 +12,17 @@ namespace XamTweet.Forms.Views
         public MainView()
         {
             InitializeComponent();
+
+            this.WhenActivated(disposables =>
+            {
+                this.WhenAnyValue(v => v.ViewModel.LoadCommand).Where(x => x != null)
+                    .Select(x => Unit.Default)
+                    .InvokeCommand(ViewModel.LoadCommand)
+                    .DisposeWith(disposables);
+
+                this.Bind(ViewModel, vm => vm.TweetText, v => v.TweetText.Text).DisposeWith(disposables);
+                this.BindCommand(ViewModel, vm => vm.PublisCommand, v => v.TweetPublish);
+            });
         }
     }
 }
