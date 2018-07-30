6e3a14050deba620bbafda617018c77b10f37900
Darío Alonso <dariovillar@hotmail.com>
2018-07-30 19:59:04 +0200

    Creando ViewModel para Tweet

---
+++ XamTweet.ViewModel/Tweet/TweetViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ReactiveUI;
+using Tweetinvi.Models;
+
+namespace XamTweet.ViewModel
+{
+    public class TweetViewModel : ReactiveObject
+    {
+        public ITweet Tweet { get; set; }
+    }
+}
