44d70ce7fe767b7fadaa4861b9bfa1a180d121d2
Darío Alonso <dariovillar@hotmail.com>
2018-07-30 19:58:38 +0200

    Creando ViewCell para mostrar Tweet

---
+++ XamTweet/XamTweet/Views/Cells/TweetCell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ReactiveUI;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamTweet.Forms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TweetCell
+    {
+        public TweetCell()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(disposables =>
+            {
+                disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.FullText, v => v.MainText.Text));
+                disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.CreatedBy.ProfileImageUrlHttps,
+                    v => v.TweetUserImage.Source, url => ImageSource.FromUri(new Uri(url))));
+            });
+        }
+    }
+}
