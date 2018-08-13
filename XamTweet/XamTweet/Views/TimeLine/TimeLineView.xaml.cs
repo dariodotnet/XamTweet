c5de5a76eaebb96962248271cad0d6d5ac72ebd1
Darío Alonso <dariovillar@hotmail.com>
2018-08-13 19:59:18 +0200

    Refactor de la vista

---
+++ XamTweet/XamTweet/Views/TimeLine/TimeLineView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace XamTweet.Forms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TimeLineView
+    {
+        public TimeLineView()
+        {
+            InitializeComponent();
+        }
+    }
+}
