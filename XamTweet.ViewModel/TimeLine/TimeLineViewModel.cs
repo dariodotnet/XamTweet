d8dabac78707d5e01b93794f10793ea0e82f4220
Darío Alonso <dariovillar@hotmail.com>
2018-09-04 20:30:03 +0200

    Creando lista para los tweets y recuperando timeline

--- XamTweet.ViewModel/TimeLine/TimeLineViewModel.cs
+++ XamTweet.ViewModel/TimeLine/TimeLineViewModel.cs
@@ -1,7 +1,37 @@
-﻿namespace XamTweet.ViewModel
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using Splat;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
+using Tweetinvi.Models;
+using XamTweet.Contracts;
+
+namespace XamTweet.ViewModel
 {
     public class TimeLineViewModel : NavigationViewModel
     {
+        private readonly ITwitterService _twitterService;
 
+        public extern ITwitterCredentials Credentials { [ObservableAsProperty] get; }
+        public extern IEnumerable<ITweet> Timeline { [ObservableAsProperty] get; }
+
+        public ReactiveCommand<Unit, ITwitterCredentials> LoginCommand { get; }
+        public ReactiveCommand<Unit, IEnumerable<ITweet>> TimelineCommand { get; }
+
+        public TimeLineViewModel(ITwitterService twitterService = null)
+        {
+            _twitterService = twitterService ?? Locator.Current.GetService<ITwitterService>();
+
+            LoginCommand = ReactiveCommand.CreateFromObservable(_twitterService.Login);
+            LoginCommand.ToPropertyEx(this, x => x.Credentials);
+
+            TimelineCommand = ReactiveCommand.CreateFromObservable(_twitterService.GetTimeline);
+            TimelineCommand.ToPropertyEx(this, x => x.Timeline);
+
+            this.WhenAnyValue(x => x.Credentials).Where(x => x != null)
+                .Select(x => Unit.Default)
+                .InvokeCommand(TimelineCommand);
+        }
     }
 }
--- XamTweet/XamTweet/Views/TimeLine/TimeLineView.xaml.cs
+++ XamTweet/XamTweet/Views/TimeLine/TimeLineView.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveUI;
+using Xamarin.Forms.Xaml;
+using XamTweet.ViewModel;
 
 namespace XamTweet.Forms.Views
 {
@@ -8,6 +14,17 @@ namespace XamTweet.Forms.Views
         public TimeLineView()
         {
             InitializeComponent();
+
+            this.WhenActivated(disposables =>
+            {
+                this.WhenAnyValue(v => v.ViewModel.LoginCommand).Where(x => x != null)
+                    .Select(x => Unit.Default)
+                    .InvokeCommand(ViewModel.LoginCommand)
+                    .DisposeWith(disposables);
+
+                this.OneWayBind(ViewModel, vm => vm.Timeline, v => v.Timeline.ItemsSource,
+                    arg => arg.ToList().Select(x => new TweetViewModel { Tweet = x })).DisposeWith(disposables);
+            });
         }
     }
 }
