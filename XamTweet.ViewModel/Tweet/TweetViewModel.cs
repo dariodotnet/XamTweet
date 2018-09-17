a733209d51411bc8b4dcae3862c0a96112880ff5
Darío Alonso <dariovillar@hotmail.com>
2018-09-17 20:03:11 +0200

    Actualizando tweet

--- XamTweet.ViewModel/Tweet/TweetViewModel.cs
+++ XamTweet.ViewModel/Tweet/TweetViewModel.cs
@@ -1,26 +1,34 @@
 ﻿using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using Splat;
 using System;
 using System.Reactive;
 using System.Reactive.Linq;
 using Tweetinvi.Models;
+using XamTweet.Contracts;
 
 namespace XamTweet.ViewModel
 {
     public class TweetViewModel : ReactiveObject
     {
-
+        private readonly ITwitterService _twitterService;
         //crear propiedades observables para gestionar el estado del tweet
 
         [Reactive] public ITweet Tweet { get; set; }
-
         [Reactive] public bool Favorited { get; set; }
         [Reactive] public int FavoriteCount { get; set; }
 
         public ReactiveCommand FavoriteCommand { get; }
 
-        public TweetViewModel()
+        public ReactiveCommand<long, ITweet> UpdateTweetCommand { get; }
+
+        public TweetViewModel(ITwitterService twitterService = null)
         {
+            _twitterService = twitterService ?? Locator.Current.GetService<ITwitterService>();
+
+            UpdateTweetCommand = ReactiveCommand.CreateFromObservable<long, ITweet>(x => _twitterService.UpdateTweet(x));
+            UpdateTweetCommand.Subscribe(ConfigureData);
+
             FavoriteCommand = ReactiveCommand.CreateFromTask(async () =>
             {
                 if (Tweet.Favorited)
@@ -28,21 +36,23 @@ namespace XamTweet.ViewModel
                 else
                     await Tweet.FavoriteAsync();
 
-                Favorited = Tweet.Favorited;
-                FavoriteCount = Tweet.Favorited ?
-                    FavoriteCount = Tweet.FavoriteCount + 1 :
-                    FavoriteCount = Tweet.FavoriteCount;
+                Observable.Start(() => Tweet.Id).InvokeCommand(UpdateTweetCommand);
 
                 return Unit.Default;
             });
 
             this.WhenAnyValue(x => x.Tweet).Where(x => x != null)
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .Subscribe(tweet =>
-                {
-                    Favorited = tweet.Favorited;
-                    FavoriteCount = tweet.FavoriteCount;
-                });
+                .ObserveOn(RxApp.TaskpoolScheduler)
+                .Subscribe(ConfigureData);
+        }
+
+        private void ConfigureData(ITweet x)
+        {
+            if (Tweet == x)
+                return;
+            Tweet = x;
+            Favorited = x.Favorited;
+            FavoriteCount = x.FavoriteCount;
         }
     }
 }
