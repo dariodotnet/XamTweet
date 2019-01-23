b314153189f1e70a09f19bd19645551be76c4340
Darío Alonso <dariovillar@hotmail.com>
2019-01-23 21:22:22 +0100

    Creado modelo para cache local

---
+++ XamTweet.Base/Models/StoredTweet.cs
@@ -0,0 +1,16 @@
+﻿namespace XamTweet
+{
+    using System;
+
+    public class StoredTweet
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public bool Favorited { get; set; }
+        public int FavoriteCount { get; set; }
+        public int Replies { get; set; }
+        public int Retweets { get; set; }
+        public string FullText { get; set; }
+    }
+}
