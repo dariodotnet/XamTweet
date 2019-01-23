8a5b297706b40ad2ec458572bacc628fc1c720f3
Darío Alonso <dariovillar@hotmail.com>
2019-01-23 18:35:46 +0100

    Eliminando comandos y conectando con servicio para listado de tweets

--- XamTweet.ViewModel/TimeLine/TimeLineViewModel.cs
+++ XamTweet.ViewModel/TimeLine/TimeLineViewModel.cs
@@ -1,49 +1,26 @@
-﻿using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
-using Splat;
-using System;
-using System.Collections.Generic;
-using System.Reactive;
-using System.Reactive.Linq;
-using Tweetinvi.Models;
-using XamTweet.Contracts;
-using XamTweet.ViewModel.TweetDetail;
-
-namespace XamTweet.ViewModel
+﻿namespace XamTweet.ViewModel
 {
+    using Contracts;
+    using DynamicData;
+    using Splat;
+    using System;
+    using System.Collections.ObjectModel;
+
     public class TimeLineViewModel : NavigationViewModel
     {
         private readonly ITwitterService _twitterService;
 
-        public extern ITwitterCredentials Credentials { [ObservableAsProperty] get; }
-        public extern IEnumerable<ITweet> Timeline { [ObservableAsProperty] get; }
-
-        [Reactive] public ITweet SelectedTweet { get; set; }
-
-        public ReactiveCommand<Unit, ITwitterCredentials> LoginCommand { get; }
-        public ReactiveCommand<Unit, IEnumerable<ITweet>> TimelineCommand { get; }
+        private readonly ReadOnlyObservableCollection<TweetViewModel> timeLine;
+        public ReadOnlyObservableCollection<TweetViewModel> TimeLine => timeLine;
 
         public TimeLineViewModel(ITwitterService twitterService = null)
         {
             _twitterService = twitterService ?? Locator.Current.GetService<ITwitterService>();
 
-            LoginCommand = ReactiveCommand.CreateFromObservable(_twitterService.Login);
-            LoginCommand.ToPropertyEx(this, x => x.Credentials);
-
-            TimelineCommand = ReactiveCommand.CreateFromObservable(_twitterService.GetTimeline);
-            TimelineCommand.ToPropertyEx(this, x => x.Timeline);
-
-            this.WhenAnyValue(x => x.Credentials).Where(x => x != null)
-                .Select(x => Unit.Default)
-                .InvokeCommand(TimelineCommand);
-
-            this.WhenAnyValue(x => x.SelectedTweet).Where(x => x != null)
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .Subscribe(tweet =>
-                {
-                    _twitterService.SetSelected(tweet);
-                    Navigator.Push<TweetDetailViewModel>();
-                });
+            _twitterService.Tweets.Connect()
+                .Transform(t => new TweetViewModel { Tweet = t })
+                .Bind(out timeLine)
+                .DisposeMany().Subscribe();
         }
     }
 }
