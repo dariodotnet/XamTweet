54f1f1b32f3ced98bf72562e018c51fb7e40858f
Darío Alonso <dariovillar@hotmail.com>
2019-01-23 21:21:00 +0100

    Cambiando binding

--- XamTweet/XamTweet/Views/Cells/TweetCell.xaml.cs
+++ XamTweet/XamTweet/Views/Cells/TweetCell.xaml.cs
@@ -1,10 +1,10 @@
-﻿using ReactiveUI;
-using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace XamTweet.Forms.Views
+﻿namespace XamTweet.Forms.Views.Cells
 {
+    using ReactiveUI;
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TweetCell
     {
@@ -15,17 +15,13 @@ namespace XamTweet.Forms.Views
             this.WhenActivated(disposables =>
             {
                 disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.FullText, v => v.MainText.Text));
-                disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.CreatedBy.ProfileImageUrlHttps,
+                disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.CreatedBy,
                     v => v.TweetUserImage.Source, url => ImageSource.FromUri(new Uri(url))));
-                disposables(this.OneWayBind(ViewModel, vm => vm.Favorited, v => v.Like.Source,
+                disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.Favorited, v => v.Like.Source,
                     arg => arg ? ImageSource.FromFile("Like.png") : ImageSource.FromFile("LikeEmpty.png")));
-                disposables(this.OneWayBind(ViewModel, vm => vm.FavoriteCount, v => v.FavoritesCount.Text));
-                disposables(this.OneWayBind(ViewModel, vm => vm.Replies, v => v.ReplyCount.Text));
-                disposables(this.OneWayBind(ViewModel, vm => vm.Retweets, v => v.RetweetCount.Text));
-
-                //disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.Favorited, v => v.TapFavorite.CommandParameter));
-
-                disposables(this.BindCommand(ViewModel, vm => vm.FavoriteCommand, v => v.TapFavorite));
+                disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.FavoriteCount, v => v.FavoritesCount.Text));
+                disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.Replies, v => v.ReplyCount.Text));
+                disposables(this.OneWayBind(ViewModel, vm => vm.Tweet.Retweets, v => v.RetweetCount.Text));
             });
         }
     }
