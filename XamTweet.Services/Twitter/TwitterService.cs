1d309b4dc7bf6adf8e470eb93e27573f409f1fd3
Darío Alonso <dariovillar@hotmail.com>
2019-01-23 21:23:21 +0100

    recuperando cache local y actualizando

--- XamTweet.Services/Twitter/TwitterService.cs
+++ XamTweet.Services/Twitter/TwitterService.cs
@@ -3,8 +3,9 @@
     using Base;
     using Contracts;
     using DynamicData;
+    using Splat;
     using System;
-    using System.Diagnostics;
+    using System.Linq;
     using System.Reactive.Linq;
     using System.Reactive.Subjects;
     using Tweetinvi;
@@ -12,45 +13,79 @@
 
     public class TwitterService : ITwitterService
     {
-        private readonly Subject<ITwitterCredentials> credentials = new Subject<ITwitterCredentials>();
+        private readonly IStorageService _storageService;
+        private readonly Subject<ITwitterCredentials> _credentials = new Subject<ITwitterCredentials>();
 
-        public SourceList<ITweet> Tweets { get; }
-
-        public TwitterService()
+        public TwitterService(IStorageService storageService = null)
         {
-            Tweets = new SourceList<ITweet>();
+            _storageService = storageService ?? Locator.Current.GetService<IStorageService>();
+            Tweets = new SourceList<StoredTweet>();
 
-            credentials.Subscribe(c => GetTimeLine());
+            _credentials.Subscribe(c => GetTimeLine());
 
             Login();
         }
 
-        private void Login()
-        {
-            credentials.OnNext(Auth.SetUserCredentials(AppKeys.ConsumerKey, AppKeys.ConsumerSecret,
-                AppKeys.UserAccessToken, AppKeys.UserAccessSecret));
-        }
-
-        private void GetTimeLine()
-        {
-            Tweets.AddRange(Timeline.GetHomeTimeline());
-        }
+        public SourceList<StoredTweet> Tweets { get; }
 
         public IObservable<ITweet> PublishTweet(string tweet)
         {
-            //return Observable.Return(Tweet.PublishTweet(tweet));
             return null;
         }
 
         public IObservable<ITweet> UpdateTweet(long id)
         {
-            Debug.WriteLine($"Pidiendo informacion sobre el tweet: {id} a las {DateTime.Now}");
             return Observable.Return(Tweet.GetTweet(id));
         }
 
         public void SetSelected(ITweet tweet)
         {
-            //Tweets.Add(tweet);
+
+        }
+
+        private void Login()
+        {
+            _credentials.OnNext(Auth.SetUserCredentials(AppKeys.ConsumerKey, AppKeys.ConsumerSecret,
+                AppKeys.UserAccessToken, AppKeys.UserAccessSecret));
+        }
+
+        private void GetTimeLine()
+        {
+            _storageService.GetAllObjects<StoredTweet>().Subscribe(tweets =>
+            {
+                Tweets.AddRange(tweets);
+                Refresh();
+            }, ex =>
+            {
+                Refresh();
+            });
+        }
+
+        private void Refresh()
+        {
+            foreach (var tweet in Timeline.GetHomeTimeline())
+            {
+                var stored = new StoredTweet
+                {
+                    Id = tweet.Id,
+                    CreatedAt = tweet.CreatedAt,
+                    CreatedBy = tweet.CreatedBy.ProfileBackgroundImageUrlHttps,
+                    FavoriteCount = tweet.FavoriteCount,
+                    Favorited = tweet.Favorited,
+                    FullText = tweet.FullText,
+                    Replies = tweet.ReplyCount ?? 0,
+                    Retweets = tweet.RetweetCount
+                };
+                _storageService.InsertObject(stored.Id.ToString(), stored, DateTimeOffset.UtcNow.AddDays(3))
+                    .Subscribe(t =>
+                    {
+                        var current = Tweets.Items.FirstOrDefault(x => x.Id.Equals(t.Id));
+                        if (current is null)
+                            Tweets.Add(t);
+                        else
+                            Tweets.Replace(current, t);
+                    });
+            }
         }
     }
 }
