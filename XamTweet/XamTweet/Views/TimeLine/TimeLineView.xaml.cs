e861b5f436a3d43ca888772b0018755873c2a408
Darío Alonso <dariovillar@hotmail.com>
2019-01-23 18:35:07 +0100

    Eliminando selección de item y dejando binding a datos

--- XamTweet/XamTweet/Views/TimeLine/TimeLineView.xaml.cs
+++ XamTweet/XamTweet/Views/TimeLine/TimeLineView.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using ReactiveUI;
-using System.Linq;
-using System.Reactive;
 using System.Reactive.Disposables;
-using System.Reactive.Linq;
 using Xamarin.Forms.Xaml;
-using XamTweet.ViewModel;
 
 namespace XamTweet.Forms.Views
 {
@@ -17,17 +13,7 @@ namespace XamTweet.Forms.Views
 
             this.WhenActivated(disposables =>
             {
-                this.WhenAnyValue(v => v.ViewModel.LoginCommand).Where(x => x != null)
-                    .Select(x => Unit.Default)
-                    .InvokeCommand(ViewModel.LoginCommand)
-                    .DisposeWith(disposables);
-
-                this.OneWayBind(ViewModel, vm => vm.Timeline, v => v.Timeline.ItemsSource,
-                    arg => arg.ToList().Select(x => new TweetViewModel { Tweet = x })).DisposeWith(disposables);
-
-                this.Bind(ViewModel, vm => vm.SelectedTweet, v => v.Timeline.SelectedItem,
-                    arg => new TweetViewModel() { Tweet = arg },
-                    arg => ((TweetViewModel)arg).Tweet).DisposeWith(disposables);
+                this.OneWayBind(ViewModel, vm => vm.TimeLine, v => v.Timeline.ItemsSource).DisposeWith(disposables);
             });
         }
     }
