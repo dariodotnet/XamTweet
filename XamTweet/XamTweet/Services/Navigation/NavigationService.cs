0bea868b5694863e56d2b3663a48915c449f0556
Darío Alonso <dariovillar@hotmail.com>
2019-01-23 18:34:04 +0100

    Eliminando Master Detail

--- XamTweet/XamTweet/Services/Navigation/NavigationService.cs
+++ XamTweet/XamTweet/Services/Navigation/NavigationService.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Linq;
-using System.Reactive;
-using System.Reactive.Linq;
-using System.Reactive.Threading.Tasks;
-using System.Reflection;
-using Xamarin.Forms;
-using XamTweet.Contracts;
-using XamTweet.ViewModel;
-
-namespace XamTweet.Forms.Services
+﻿namespace XamTweet.Forms.Services
 {
+    using Contracts;
+    using System;
+    using System.Linq;
+    using System.Reactive;
+    using System.Reactive.Threading.Tasks;
+    using System.Reflection;
+    using ViewModel;
+    using Xamarin.Forms;
+
     public class NavigationService : INavigationService
     {
-        //Implementar gestion de errores
-
         public void LoadMainView<T>() where T : class, IViewModel
         {
             var ctor = typeof(T).GetTypeInfo().DeclaredConstructors.First();
@@ -45,17 +42,7 @@ namespace XamTweet.Forms.Services
 
         public void LoadMainView()
         {
-            var menu = new MenuViewModel().GetView();
-            menu.Title = " ";
-            menu.Icon = "Menu.png";
-
-            var masterDetail = new MasterDetailPage
-            {
-                Detail = new MainViewModel().GetView(),
-                Master = menu
-            };
-
-            Application.Current.MainPage = masterDetail;
+            Application.Current.MainPage = new NavigationPage(new TimeLineViewModel().GetView());
         }
 
         public INavigation Navigator => ((MasterDetailPage)Application.Current.MainPage).Detail.Navigation;
