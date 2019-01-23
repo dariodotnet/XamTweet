4a70bfd2ffad8de83ee118eb1477b91edd009c11
Darío Alonso <dariovillar@hotmail.com>
2019-01-23 18:37:14 +0100

    Eliminando vistas no utilizadas

--- XamTweet/XamTweet/AppBootstrap.cs
+++ XamTweet/XamTweet/AppBootstrap.cs
@@ -26,12 +26,7 @@ namespace XamTweet.Forms
 
         private void InitViews()
         {
-            Locator.CurrentMutable.Register(() => new MainView(), typeof(IViewFor<MainViewModel>));
-            Locator.CurrentMutable.Register(() => new MenuView(), typeof(IViewFor<MenuViewModel>));
             Locator.CurrentMutable.Register(() => new TimeLineView(), typeof(IViewFor<TimeLineViewModel>));
-            Locator.CurrentMutable.Register(() => new SearchView(), typeof(IViewFor<SearchViewModel>));
-            Locator.CurrentMutable.Register(() => new NotificationsView(), typeof(IViewFor<NotificationsViewModel>));
-            Locator.CurrentMutable.Register(() => new MessagesView(), typeof(IViewFor<MessagesViewModel>));
             Locator.CurrentMutable.Register(() => new TweetDetailView(), typeof(IViewFor<TweetDetailViewModel>));
         }
 
