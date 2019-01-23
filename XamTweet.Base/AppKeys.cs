7273c97515baa4eddf298549afa2c16c45607475
Darío Alonso <dariovillar@hotmail.com>
2019-01-23 12:46:09 +0100

    Datos base de la aplicación

---
+++ XamTweet.Base/AppKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace XamTweet.Base
+{
+    public static class AppKeys
+    {
+        public static string ConsumerKey => "CONSUMER_KEY";
+        public static string ConsumerSecret => "CONSUMER_SECRET";
+        public static string UserAccessToken => "USER_ACCESS_TOKEN";
+        public static string UserAccessSecret => "USER_ACCESS_SECRET";
+    }
+}
