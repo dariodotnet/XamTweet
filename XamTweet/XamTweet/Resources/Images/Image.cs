32562100bee322158afba8d7dd294da99989362d
Darío Alonso <dariovillar@hotmail.com>
2019-01-24 12:16:25 +0100

    Recursos globales

---
+++ XamTweet/XamTweet/Resources/Images/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace XamTweet.Forms.Resources.Images
+{
+    public static class Image
+    {
+        public const string DefaultUser = "XamTweet.Forms.Resources.Images.DefaultUser.png";
+        public const string Like = "XamTweet.Forms.Resources.Images.Like.png";
+        public const string LikeEmpty = "XamTweet.Forms.Resources.Images.LikeEmpty.png";
+        public const string Reply = "XamTweet.Forms.Resources.Images.Reply.png";
+        public const string Retweet = "XamTweet.Forms.Resources.Images.Retweet.png";
+        public const string Share = "XamTweet.Forms.Resources.Images.Share.png";
+    }
+}
